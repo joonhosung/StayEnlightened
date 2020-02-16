cc3f804755216750ca8014be4a674635d5fb5450
Seangottarun <sean.wu20s@outlook.com>
2020-02-16 11:13:33 -0500

    First working website with mongodb database. Website name is not properly aliased yet though

--- webapp/client/src/api/index.js
+++ webapp/client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://server-dot-stayenlightenedv2.appspot.com/api',
 })
 
 export const getStudyspaces = () => api.get(`/studyspaces`)
