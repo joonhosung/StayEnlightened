7f7ecaee65c075e8da5254962d30d6fd9560e774
iWebster28 <iWebster28@users.noreply.github.com>
2020-02-16 03:50:48 -0500

    Camera.py and camera-call-test.py

--- image-capture/camera-call-test.py
+++ image-capture/camera-call-test.py
@@ -1,7 +1,9 @@
-import Camera
+from Camera import Camera
 import time
 
+camera = Camera(0, 'saved_img.jpg') #no self 
+
 for x in range(0, 3):
-	time.sleep(7) #Wait 7 secs to take another picture
-	Camera.capture()
+	camera.capture()
 	print("Took a picture.")
+	time.sleep(7) #Wait 7 secs to take another picture
