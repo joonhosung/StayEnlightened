4f37a37675d88829d0f601cd4e7d132201000724
jordanhong <jordanhong89@gmail.com>
2020-02-15 23:50:17 -0500

    Working code on Win 10 laptop

--- image-capture/imag-cap3.py
+++ image-capture/imag-cap3.py
@@ -7,7 +7,7 @@ while(True):
     ret, frame = cap.read()
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
     cv2.imshow('frame', rgb)
-    if cv2.waitKey(1) & OxFF == ord('q'):
+    if cv2.waitKey(1) & 255 == ord('q'):
         out = cv2.imwrite('capture.jpg', frame)
         break
 cap.release()
