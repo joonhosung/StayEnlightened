b6cb9d5fc19818104f2e14cf3af78e023ec09c06
Dragon <jordan.hong@mail.utoronto.ca>
2020-02-16 04:48:54 +0000

    Edited computer.py in serial_communications and motion_sensor_ino in sensor/motion-sensor to work with the DragonBoard

--- serial_commnication/computer.py
+++ serial_commnication/computer.py
@@ -1,17 +1,17 @@
 ## reads in data from Arduino
 import serial
 from time import sleep
-ser = serial.Serial('COM6', 9600) # Establish the connection on a specific port
+ser = serial.Serial('/dev/tty96B0', 9600) # Establish the connection on a specific port
 i=0
-while i<10:
+while i<1000:
 	
 	msg=str(ser.readline().strip().decode('ascii')) # reads from Arduino
 	print("message: "+msg)
 	sleep(.1) # Delay for one tenth of a second
 	if (msg=='0'):
-		print("starting")
+		print("No motion detected. Turning off camera.")
 	elif (msg=='1'):
-		print("turn on")
+		print("Detected motion. Turning on camera.")
 	else:
-		print("turn off")
+		print("No update from camera.")
 	i+=1
