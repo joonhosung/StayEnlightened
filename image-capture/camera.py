09f47f99cef7aced22feee6b495c67f64338b91c
iWebster28 <iWebster28@users.noreply.github.com>
2020-02-16 01:31:56 -0500

    Updated camera.py

--- image-capture/camera.py
+++ image-capture/camera.py
@@ -4,9 +4,12 @@ def capture():
 	# this function receives no parameters
 	# returns cv2 im file (so the one we passed into imsave)
 	webcam = cv2.VideoCapture(0);
-	check, frame = webcam.read();
+	check, frame = webcam.read(img);
 	cv2.imshow("Capturing", frame);
-	cv2.imwrite(filename='saved_img.jpg', img = frame)
+	#cv2.imwrite(filename='saved_img.jpg', img = frame)
+
+	#check, frame = cv2.VideoCapture.read(img) #grab, decode and return next frame from video
+	
 	webcam.release()
 	cv2.destroyAllWindows()
 	return img
