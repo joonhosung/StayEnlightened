cf92dd01c5ea7bac56fb7482091a67fbf350e9e1
Dragon <jordan.hong@mail.utoronto.ca>
2020-02-16 08:17:06 +0000

    updates to motion_sensor_ino

--- sensor/motion-sensor/motion_sensor_ino/motion_sensor_ino.ino
+++ sensor/motion-sensor/motion_sensor_ino/motion_sensor_ino.ino
@@ -2,33 +2,43 @@
 // 1. Turn on the camera if motion/people are detected
 // 2. Turn off the camera if no motion is detected.
 
-int motionPin = 13; //Output pin - if motion detected
+int lightPin = 11; //The "lights" to be controlled
+int motionPin = 13; //Diagnostic Output pin - if motion detected
 int inputPin = 2; //Input pin - the PIR sensor
 int pirState = LOW; //No motion at start
 int movement = 0; //Pin status (HIGH or LOW)
 
 void setup() {
   pinMode(motionPin, OUTPUT); 
+  pinMode(lightPin, OUTPUT);
   pinMode(inputPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-
   movement = digitalRead(inputPin); //Read input 
+  
   if (movement == HIGH) { //If motion detected:
-    digitalWrite(motionPin, HIGH); //Turn output ON
+    //digitalWrite(lightPin, HIGH); //Turn on the lights.
+    digitalWrite(motionPin, HIGH); //Turn diagnostic output ON
+    
+    //If previous state was "no motion detected", then change state to "motion detected"
     if (pirState == LOW) {
-      Serial.println(1);
+      Serial.println(1); //Send serial data to python -> computer.py, which will tell the camera to turn on and take pictures in OpenCV
       //Serial.println("Motion detected. Switching to camera.");
       pirState = HIGH; //Change state.
     }
+    
   } else { //Motion not detected:
-    digitalWrite(motionPin, LOW); //Turn output OFF
+    digitalWrite(motionPin, LOW); //Turn diagnostic output OFF
+    
+    //If last state was "motion detected", change to "no motion detected"
     if (pirState == HIGH){
       Serial.println(0);
       //Serial.println("No motion. Camera off.");
-      pirState = LOW;
+      pirState = LOW; //Change state
     }
   }
+  
+  
 }
